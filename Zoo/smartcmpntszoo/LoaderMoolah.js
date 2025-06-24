9c5890ce97c027c4b23b6a3583755fbadd23eb73
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-06-24 17:53:05 +0300

    init

---
+++ Zoo/smartcmpntszoo/LoaderMoolah.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Animated,
+  Dimensions,
+  ImageBackground,
+  ActivityIndicator,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { zoo } from '../smartconstszoo/smartstyles';
+import { lion } from '../smartimprtszoo/smartimgszoo';
+
+const { height } = Dimensions.get('window');
+
+const LoaderMoolah = () => {
+  const navigation = useNavigation();
+
+  const lionOpacity = useRef(new Animated.Value(0)).current;
+  const titleScale = useRef(new Animated.Value(0.5)).current;
+  const buttonOpacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(lionOpacity, {
+        toValue: 1,
+        duration: 1700,
+        useNativeDriver: true,
+      }),
+      Animated.spring(titleScale, {
+        toValue: 1,
+        friction: 4,
+        useNativeDriver: true,
+      }),
+      Animated.timing(buttonOpacity, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        style={{ flex: 1, width: '100%', height: '100%' }}
+        source={require('../smartasstszoo/smartimageszoo/zoobackgroundmoolah.png')}
+      >
+        <Animated.Image
+          source={lion}
+          style={[zoo.lion, { opacity: lionOpacity }]}
+        />
+
+        <Animated.Text
+          style={[
+            zoo.mainTitle,
+            {
+              transform: [{ scale: titleScale }],
+              fontSize: 45,
+              lineHeight: 50,
+              textAlign: 'center',
+              position: 'absolute',
+              top: height * 0.17,
+              alignSelf: 'center',
+            },
+          ]}
+        >
+          Smart Mooolah Zoo
+        </Animated.Text>
+
+        <ActivityIndicator
+          size={'large'}
+          color="#fff"
+          style={{
+            position: 'absolute',
+            top: height * 0.4,
+            alignSelf: 'center',
+          }}
+        />
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default LoaderMoolah;
---
+++ Zoo/smartcmpntszoo/Smartsplashzoo.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef } from 'react';
+import { View, Text, TouchableOpacity, Animated, Dimensions,Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { zoo } from '../smartconstszoo/smartstyles';
+import { lion } from '../smartimprtszoo/smartimgszoo';
+
+const { height } = Dimensions.get('window');
+
+const Smartsplashzoo = () => {
+    const navigation = useNavigation();
+
+    const lionOpacity = useRef(new Animated.Value(0)).current;
+    const titleScale = useRef(new Animated.Value(0.5)).current;
+    const buttonOpacity = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.sequence([
+            Animated.timing(lionOpacity, {
+                toValue: 1,
+                duration: 700,
+                useNativeDriver: true,
+            }),
+            Animated.spring(titleScale, {
+                toValue: 1,
+                friction: 4,
+                useNativeDriver: true,
+            }),
+            Animated.timing(buttonOpacity, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true,
+            }),
+        ]).start();
+    }, []);
+
+    return (
+        <View style={zoo.container}>
+
+            <Image
+                source={lion}
+                style={zoo.lion}
+            />
+
+            <Text
+                style={
+                    zoo.mainTitle}
+            >
+                Smart Mooolah Zoo
+            </Text>
+
+            <Animated.View
+                style={{
+                    width: '100%',
+                    opacity: buttonOpacity,
+                    position: 'absolute',
+                    bottom: 50,
+                    alignSelf: 'center'
+                }}
+            >
+                <TouchableOpacity
+                    style={zoo.button}
+                    onPress={() => navigation.navigate('Smartmiddlezoo')}
+                >
+                    <Text style={zoo.buttonText}>Continue</Text>
+                </TouchableOpacity>
+            </Animated.View>
+            
+        </View>
+    )
+};
+
+export default Smartsplashzoo;
